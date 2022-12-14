176b4bf0626319536442ed05dc919becb0b7955b
devCJH <http-@naver.com>
2022-12-14 19:59:51 +0900

    BeforeActionInterceptor 구현. BeforeActionInterceptor registry등록. Rq객체를 BeforeActionInterceptor에서 한번만 생성

---
+++ src/main/java/com/cjh/exam/demo/MyWebMvcConfigurer.java
@@ -0,0 +1,26 @@
+package com.cjh.exam.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.cjh.exam.demo.interceptor.BeforeActionInterceptor;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+
+	private BeforeActionInterceptor beforeActionInterceptor;
+
+	@Autowired
+	public MyWebMvcConfigurer(BeforeActionInterceptor beforeActionInterceptor) {
+		this.beforeActionInterceptor = beforeActionInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
+				.excludePathPatterns("/error");
+	}
+
+}
---
+++ src/main/java/com/cjh/exam/demo/interceptor/BeforeActionInterceptor.java
@@ -0,0 +1,23 @@
+package com.cjh.exam.demo.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.cjh.exam.demo.vo.Rq;
+
+@Component
+public class BeforeActionInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+		
+		Rq rq = new Rq(req);
+		req.setAttribute("rq", rq);
+		
+		return HandlerInterceptor.super.preHandle(req, resp, handler);
+	}
+
+}
