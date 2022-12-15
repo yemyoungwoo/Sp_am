88655263c1934255906eb93f1df8a7984fbb188a
yemyoungwoo <tkddlektks@gmail.com>
2022-12-15 21:26:23 +0900

    로그인, 로그아웃 Rq적용, 로그아웃 후 메인페이지로 이동

--- src/main/java/com/ymw/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/ymw/exam/demo/controller/UsrMemberController.java
@@ -1,6 +1,6 @@
 package com.ymw.exam.demo.controller;
 
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,22 +11,19 @@ import com.ymw.exam.demo.service.MemberService;
 import com.ymw.exam.demo.util.Utility;
 import com.ymw.exam.demo.vo.Member;
 import com.ymw.exam.demo.vo.ResultData;
+import com.ymw.exam.demo.vo.Rq;
 
 @Controller
 public class UsrMemberController {
-
 	private MemberService memberService;
-
 	@Autowired
 	public UsrMemberController(MemberService memberService) {
 		this.memberService = memberService;
 	}
-
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
 	public ResultData<Member> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
 			String email) {
-
 		if (Utility.empty(loginId)) {
 			return ResultData.from("F-1", "아이디를 입력해주세요");
 		}
@@ -45,18 +42,13 @@ public class UsrMemberController {
 		if (Utility.empty(email)) {
 			return ResultData.from("F-6", "이메일을 입력해주세요");
 		}
-
 		ResultData<Integer> doJoinRd = memberService.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
-
 		if (doJoinRd.isFail()) {
 			return ResultData.from(doJoinRd.getResultCode(), doJoinRd.getMsg());
 		}
-
 		Member member = memberService.getMemberById((int) doJoinRd.getData1());
-
 		return ResultData.from(doJoinRd.getResultCode(), doJoinRd.getMsg(), "member", member);
 	}
-
 	@RequestMapping("/usr/member/login")
 	public String showLogin() {
 		return "usr/member/login";
@@ -64,9 +56,11 @@ public class UsrMemberController {
 
 	@RequestMapping("/usr/member/doLogin")
 	@ResponseBody
-	public String doLogin(HttpSession httpSession, String loginId, String loginPw) {
+	public String doLogin(HttpServletRequest req, String loginId, String loginPw) {
 
-		if (httpSession.getAttribute("loginedMemberId") != null) {
+		Rq rq = (Rq) req.getAttribute("rq");
+
+		if (rq.getLoginedMemberId() != 0) {
 			return Utility.jsHistoryBack("이미 로그인 되어있습니다");
 		}
 		if (Utility.empty(loginId)) {
@@ -75,32 +69,32 @@ public class UsrMemberController {
 		if (Utility.empty(loginPw)) {
 			return Utility.jsHistoryBack("비밀번호를 입력해주세요");
 		}
-
 		Member member = memberService.getMemberByLoginId(loginId);
-
 		if (member == null) {
 			return Utility.jsHistoryBack("존재하지 않는 아이디입니다");
 		}
-
 		if (member.getLoginPw().equals(loginPw) == false) {
 			return Utility.jsHistoryBack("비밀번호가 일치하지 않습니다");
 		}
 
-		httpSession.setAttribute("loginedMemberId", member.getId());
+		rq.login(member);
+
 		return Utility.jsReplace(Utility.f("%s님 환영합니다", member.getNickname()), "/");
 	}
 
 	@RequestMapping("/usr/member/doLogout")
 	@ResponseBody
-	public ResultData doLogout(HttpSession httpSession) {
+	public String doLogout(HttpServletRequest req) {
 
-		if (httpSession.getAttribute("loginedMemberId") == null) {
-			return ResultData.from("F-1", "로그아웃 상태입니다");
+		Rq rq = (Rq) req.getAttribute("rq");
+
+		if (rq.getLoginedMemberId() == 0) {
+			return Utility.jsHistoryBack("로그아웃 상태입니다");
 		}
 
-		httpSession.removeAttribute("loginedMemberId");
+		rq.logout();
 
-		return ResultData.from("S-1", "로그아웃 되었습니다");
+		return Utility.jsReplace("로그아웃 되었습니다", "/"); 
 	}
 
 }
--- src/main/java/com/ymw/exam/demo/vo/Rq.java
+++ src/main/java/com/ymw/exam/demo/vo/Rq.java
@@ -15,26 +15,26 @@ public class Rq {
 	private int loginedMemberId;
 	private HttpServletRequest req;
 	private HttpServletResponse resp;
+	private HttpSession session;
+
 	public Rq(HttpServletRequest req, HttpServletResponse resp) {
 		this.req = req;
 		this.resp = resp;
-		
-		HttpSession httpSession = req.getSession();
-		
+		this.session = req.getSession();
+
 		int loginedMemberId = 0;
-		
-		if(httpSession.getAttribute("loginedMemberId") != null) {
-			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+
+		if(session.getAttribute("loginedMemberId") != null) {
+			loginedMemberId = (int) session.getAttribute("loginedMemberId");
 		}
-		
+
 		this.loginedMemberId = loginedMemberId;
 	}
 	public void jsPrintHistoryBack(String msg) throws IOException{
 		resp.setContentType("text/html; charset=UTF-8");
-
+		
 		print(Utility.jsHistoryBack(msg));
 	}
-
 	private void print(String str) {
 		try {
 			resp.getWriter().append(str);
@@ -43,5 +43,12 @@ public class Rq {
 		}
 	}
 
+	public void login(Member member) {
+		session.setAttribute("loginedMemberId", member.getId());
+	}
+
+	public void logout() {
+		session.removeAttribute("loginedMemberId");
+	}
 
 }
